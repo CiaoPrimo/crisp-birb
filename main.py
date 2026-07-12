d940a1e412740b6afe07cacac1e425b5511358da
CiaoPrimo <emeraldaltb@gmail.com>
2026-07-12 15:15:28 -0400

    Restore dotenv and logging setup in main.py

--- main.py
+++ main.py
@@ -451,7 +451,7 @@ class Client(commands.AutoShardedBot):
     async def _set_custom_status(self):
         activity2 = discord.CustomActivity(name=f"{STATUS}")
         if STATUS:
-            await self.change_presence(status=discord.Status.dnd, activity=activity2)
+            await self.change_presence(status=discord.Status.online, activity=activity2)
         else:
             logger.warning(
                 "[⚠️] STATUS not defined in .env, bot will not set a custom status."
